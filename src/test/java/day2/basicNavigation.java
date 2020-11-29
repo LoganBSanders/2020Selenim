18fe08442aa5f4d8662ccc847387b2425a2b5e88
LoganBSanders <loganbsanders@gmail.com>
2020-11-28 19:24:29 -0600

    Second commitwa

--- src/test/java/day2/basicNavigation.java
+++ src/test/java/day2/basicNavigation.java
@@ -13,7 +13,7 @@ public class basicNavigation {
         String expectedTitle = "Google";
         System.out.println("Title is...."+title);
         if (expectedTitle.equals(title)){
-            System.out.println("TESTt PASSED");
+            System.out.println("TaESTt PASSED");
         }
         else {
             System.out.println("TEST FAILED");
