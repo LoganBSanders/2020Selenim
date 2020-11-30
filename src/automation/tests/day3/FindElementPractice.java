8b432c67ffa6fc87f54996ef2a580f50e112802b
LoganBSanders <loganbsanders@gmail.com>
2020-11-29 18:02:19 -0600

    5 committ

--- src/automation/tests/day3/FindElementPractice.java
+++ src/automation/tests/day3/FindElementPractice.java
@@ -18,18 +18,30 @@ public class FindElementPractice {
         WebDriver driver = DriverFactory.createDriver("chrome");
 
         driver.get("http://practice.cybertekschool.com/sign_up");
-
+        Thread.sleep(2000);
         WebElement fullName = driver.findElement(By.name("full_name"));
 
         fullName.sendKeys("Ozan Doganay");
-
+        Thread.sleep(2000);
         WebElement email = driver.findElement(By.name("email"));
         email.sendKeys("odo@gmail.com");
-
+        Thread.sleep(2000);
         WebElement signUp = driver.findElement(By.name("wooden_spoon"));
         signUp.click();
 
-        Thread.sleep(3000);
+        Thread.sleep(2000);
+
+
+        String expected = "Thank you for signing up. Click the button below to return to the home page.";
+        WebElement message = driver.findElement(By.className("subheader"));
+        String actual = message.getText();
+
+        if (expected.equalsIgnoreCase(actual)){
+            System.out.println("Passed");
+        }else{
+            System.out.println("failed");
+        }
+        Thread.sleep(2000);
 
         driver.quit();
     }
